24fd192aeedbfa6a70b4f5f3137f23bdf678161a
nicolesy <nicole@nicolesy.com>
2019-11-18 09:22:11 -0800

    Added profile and about pages

--- lg_app/urls.py
+++ lg_app/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("", views.index, name = "index"),
     path("get_presets/", views.get_presets, name = "get_presets"),
     path("upload_photo/", views.upload_photo, name = "upload_photo"),
+    path("profile/", views.profile_page, name = "profile_page"),
+    path("about/", views.about_page, name = "about_page"),
 ]
