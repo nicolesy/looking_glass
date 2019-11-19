fec3c03bf751e9b2911723fa45c3877dbe91fb73
nicolesy <nicole@nicolesy.com>
2019-11-18 16:24:44 -0800

    Added updated "new_context" to work with dropdown list

--- lg_app/views.py
+++ lg_app/views.py
@@ -18,20 +18,50 @@ def index(request):
     presets = Preset.objects.all().order_by("preset_name")
     preset_packs = PresetPack.objects.all().order_by("pack_name")
     images = UploadedImage.objects.all()
-    latest_image = UploadedImage.objects.order_by('timestamp').last()
-    processed_images = latest_image.processed_images.all()
     
+    latest_image = request.user.uploaded_images.order_by('timestamp').last()
+    if latest_image == None:
+        return render(request, "lg_app/index.html", {})
+    else:
+        processed_images = latest_image.processed_images.all()
     
-    context = {
+    # context = {
+    #     "preset_packs": preset_packs,
+    #     "presets": presets,
+    #     "images": images,
+    #     "latest_image": latest_image,
+    #     "processed_images": processed_images,
+    # }
+    
+    data_packs = []
+    for preset_pack in preset_packs:
+        presets = []
+        for preset in preset_pack.presets.all():
+            presets.append({
+                "name": preset.preset_name,
+                "processed_image": ProcessedImage.objects.get(preset_id=preset.id, user_upload_id=latest_image.id).image.url
+            })
+        data_packs.append({
+            "name": preset_pack.pack_name,
+            "url": preset_pack.pack_url,
+            "description": preset_pack.pack_description,
+            "cover": preset_pack.pack_cover,
+            "fave": preset_pack.pack_fave,
+            "presets": presets,
+        })
+    
+
+    new_context = {
         "preset_packs": preset_packs,
-        "presets": presets,
         "images": images,
         "latest_image": latest_image,
         "processed_images": processed_images,
+        "data_packs": data_packs,
     }
+
     
     if request.user.is_authenticated:
-        return render(request, "lg_app/index.html", context)
+        return render(request, "lg_app/index.html", new_context)
     else:
         return HttpResponseRedirect(reverse('users:login_register'))
 
@@ -72,10 +102,7 @@ def get_presets(request):
 # upload a photo and the processed photos are added to the database
 @login_required
 def upload_photo(request):
-    
-    request.user.user_img.all().delete() # UploadedImages for the users
-
-    
+    request.user.uploaded_images.all().delete() # UploadedImages for the users
     upload_photo = request.FILES["upload_photo"]
     user = request.user
     code = ''.join([choice(ascii_letters + digits) for i in range(50)])
@@ -84,13 +111,13 @@ def upload_photo(request):
         user = user,
         code = code,
     )
-    new_photo.save()
     
+    new_photo.save()
     presets = Preset.objects.all()
     
     
     for preset in presets:
-        basewidth = 800 #determines the image width
+        basewidth = 800 #determines the image width ###
         lut = pillow_lut.load_cube_file(preset.preset_file.path)
         image = Image.open(new_photo.user_img.path)
         image = image.filter(lut)
@@ -131,3 +158,18 @@ def watermark(photo, text, pos):
     font = ImageFont.truetype("HelveticaNeue.ttc", 24)
     drawing.text(pos, text, fill=white, font=font)
     return photo
+
+
+def select_presets(request):
+    presets_get = request.POST.get("preset_dropdown") #this gets the name from the select list
+    print("=" *100)
+    print(presets_get)
+    print("=" *100)
+    preset_packs = PresetPack.objects.all().filter(presets_get)
+    presets = Preset.objects.all().order_by("preset_name")
+    processed_images = latest_image.processed_images.all()
+    
+    context = {}
+    
+    
+    return render(request, "lg_app/index.html", context)
