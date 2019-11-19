c8872f9b50364b85925edc3a170a4f7813ad0748
nicolesy <nicole@nicolesy.com>
2019-11-19 14:44:22 -0800

    Added "add_fave" path to work with the favorites form/button

--- lg_app/urls.py
+++ lg_app/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path("profile/", views.profile_page, name = "profile_page"),
     path("about/", views.about_page, name = "about_page"),
     path("select_presets/", views.select_presets, name = "select_presets"),
+    path('add_fave/<int:pack_id>/', views.add_fave, name='add_fave'),
 ]
