a8ebf7638c6e8c33a87aa38c8bf296c61adbeed4
nicolesy <nicole@nicolesy.com>
2019-11-28 17:55:43 -0800

    Misc updates

--- lg_app/urls.py
+++ lg_app/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 app_name = "lg_app"
 urlpatterns = [
     path("", views.index, name = "index"),
-    path("get_presets/", views.get_presets, name = "get_presets"),
+    # path("get_presets/", views.get_presets, name = "get_presets"),
     path("upload_photo/", views.upload_photo, name = "upload_photo"),
     path("profile/", views.profile_page, name = "profile_page"),
     path("about/", views.about_page, name = "about_page"),
