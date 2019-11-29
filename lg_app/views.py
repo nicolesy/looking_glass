fb6dd15134a1c692728b49de459b7b38564c0b33
nicolesy <nicole@nicolesy.com>
2019-11-28 17:56:12 -0800

    More optimized: presets only process when selected via dropdown

--- lg_app/views.py
+++ lg_app/views.py
@@ -15,62 +15,76 @@ from random import choice
 import datetime
 
 
-
 def index(request):
-    presets = Preset.objects.all().order_by("preset_name")
     preset_packs = PresetPack.objects.all().order_by("pack_name")
     images = UploadedImage.objects.all()
+    latest_image = request.user.uploaded_images.order_by('timestamp').last()
     
     if not request.user.is_authenticated:
         return HttpResponseRedirect(reverse('users:login_register'))
-    
-    latest_image = request.user.uploaded_images.order_by('timestamp').last()
-    
+
     if latest_image == None:
         return render(request, "lg_app/index.html", {})
+    
+    processed_images = [] # OLD CODE = latest_image.processed_images.all()
+    # existing_images = latest_image.processed_images.all()
+        
+    if request.method == "POST":
+        get_pack_id = request.POST.get("preset_pack_id")
+        uploaded_image = request.user.uploaded_images.order_by('timestamp').last()
+        code = ''.join([choice(ascii_letters + digits) for i in range(50)])
+        user = request.user
+        
+        pack = PresetPack.objects.get(id=get_pack_id)
+        presets = pack.presets.all()
+        
+        for preset in presets:
+            # check if a processed image already exists for this uploaded image and preset
+            existing_image = ProcessedImage.objects.filter(user_upload=uploaded_image, preset=preset).first()
+            
+            if existing_image is not None:
+                processed_images.append(existing_image) # appends the list with existing processed images (prevents re-processing)
+            
+            else:
+                lut = pillow_lut.load_cube_file(preset.preset_file.path)
+                image = Image.open(uploaded_image.user_img.path)
+                image = image.filter(lut)
+                watermark(image, text=(preset.preset_name + " | nicolesy.com"), pos=(30, 30))
+                
+                image = image.convert('RGB')
+                output = BytesIO()
+                image.save(output, format='JPEG', quality=100)
+                output.seek(0)
+                image = InMemoryUploadedFile(output, 'ImageField',
+                                            'image.jpg',
+                                            'image/jpeg',
+                                            sys.getsizeof(output), None)
+                
+                preset_applied_image = ProcessedImage(
+                    image = image,
+                    preset = preset,
+                    user_upload = uploaded_image,
+                    code = code,                    
+                )
+                preset_applied_image.save()
+                processed_images.append(preset_applied_image)
+    
     else:
-        processed_images = latest_image.processed_images.all()
+        pack = None # this needs to be here or 'pack' shows up as not defined
+        
     
-    # context = {
-    #     "preset_packs": preset_packs,
-    #     "presets": presets,
-    #     "images": images,
-    #     "latest_image": latest_image,
-    #     "processed_images": processed_images,
-    # }
-    
-    data_packs = []
-    for preset_pack in preset_packs:
-        presets = []
-        for preset in preset_pack.presets.all():
-            processed_image = ProcessedImage.objects.filter(preset_id=preset.id, user_upload_id=latest_image.id).first()
-            if processed_image is not None:
-                presets.append({
-                    "name": preset.preset_name,
-                    "processed_image": ProcessedImage.objects.get(preset_id=preset.id, user_upload_id=latest_image.id).image.url
-                })
-        data_packs.append({
-            "id": preset_pack.id,
-            "name": preset_pack.pack_name,
-            "url": preset_pack.pack_url,
-            "description": preset_pack.pack_description,
-            "cover": preset_pack.pack_cover,
-            "fave": preset_pack.pack_fave.all(),
-            "presets": presets,
-            "price": preset_pack.pack_price,
-        })
-        # print("=" *100)
-        # print(preset_pack.pack_fave.all())
-        # print("=" *100)
-    
-
-    new_context = {
+    context = {
         "preset_packs": preset_packs,
         "images": images,
         "latest_image": latest_image,
         "processed_images": processed_images,
-        "data_packs": data_packs,
+        "pack": pack,
     }
+    
+    if request.user.is_authenticated:
+        return render(request, "lg_app/index.html", context)
+    else:
+        return HttpResponseRedirect(reverse('users:login_register'))
 
     
     if request.user.is_authenticated:
@@ -98,18 +112,18 @@ def about_page(request):
 
     
 # this is not finished and NEEDS WORK
-def get_presets(request):
-    db_presets = request.Preset.preset_pack.order_by("preset_name")
-    presets = []
-    for db_preset in db_presets:
-        preset = db_presets.preset_name
-        thumbnail = db_presets.preset_thumbnail
-        presets.append ({
-            "preset": preset,
-            "thumbnail": thumbnail,
-        })
-    
-    return JsonResponse({"presets": presets})
+# def get_presets(request):
+#     db_presets = request.Preset.preset_pack.order_by("preset_name")
+#     presets = []
+#     for db_preset in db_presets:
+#         preset = db_presets.preset_name
+#         thumbnail = db_presets.preset_thumbnail
+#         presets.append ({
+#             "preset": preset,
+#             "thumbnail": thumbnail,
+#         })
+# 
+#     return JsonResponse({"presets": presets})
 
 
 # upload a photo and the processed photos are added to the database
@@ -135,41 +149,7 @@ def upload_photo(request):
     hsize = int((float(image.size[1])*float(wpercent))) #resizes
     image = image.resize((basewidth,hsize), Image.ANTIALIAS) #resizes
     image.save(new_photo.user_img.path, format='JPEG', quality=50)
-    #####
     
-    presets = Preset.objects.all()
-    
-    for preset in presets:
-        print("=" *50)
-        print(datetime.datetime.now())
-        print("=" *100)
-        # basewidth = 800 #determines the image width ###
-        lut = pillow_lut.load_cube_file(preset.preset_file.path)
-        image = Image.open(new_photo.user_img.path)
-        image = image.filter(lut)
-        # wpercent = (basewidth/float(image.size[0])) #resizes
-        # hsize = int((float(image.size[1])*float(wpercent))) #resizes
-        # image = image.resize((basewidth,hsize), Image.ANTIALIAS) #resizes
-        
-        watermark(image, text=(preset.preset_name + " | nicolesy.com"), pos=(30, 30))
-        
-        image = image.convert('RGB')
-        output = BytesIO()
-        image.save(output, format='JPEG', quality=100)
-        output.seek(0)
-        image = InMemoryUploadedFile(output, 'ImageField',
-                                    'image.jpg',
-                                    'image/jpeg',
-                                    sys.getsizeof(output), None)
-        
-        preset_applied_image = ProcessedImage(
-            image = image,
-            preset = preset,
-            user_upload = new_photo,
-            code = code,
-            
-        )
-        preset_applied_image.save()
     
     return HttpResponseRedirect(reverse('lg_app:index'))
 
@@ -193,7 +173,6 @@ def select_presets(request):
     
     context = {}
     
-    
     return render(request, "lg_app/index.html", context)
     
 
@@ -202,6 +181,4 @@ def add_fave(request, pack_id):
     pack = PresetPack.objects.get(id=pack_id) 
     pack.pack_fave.add(request.user)
     
-    
     return HttpResponseRedirect(reverse('lg_app:profile_page'))
-    
