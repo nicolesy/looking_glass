9afd8aaa23dd4cb8bb1e18d8dbff04b0f0891ce6
nicolesy <nicole@nicolesy.com>
2019-11-30 15:37:35 -0800

    Added delete_fave view

--- lg_app/urls.py
+++ lg_app/urls.py
@@ -4,10 +4,10 @@ from django.urls import path
 app_name = "lg_app"
 urlpatterns = [
     path("", views.index, name = "index"),
-    # path("get_presets/", views.get_presets, name = "get_presets"),
     path("upload_photo/", views.upload_photo, name = "upload_photo"),
     path("profile/", views.profile_page, name = "profile_page"),
     path("about/", views.about_page, name = "about_page"),
     path("select_presets/", views.select_presets, name = "select_presets"),
     path('add_fave/<int:pack_id>/', views.add_fave, name='add_fave'),
+    path('delete_fave/<int:pack_id>/', views.delete_fave, name='delete_fave'),
 ]
