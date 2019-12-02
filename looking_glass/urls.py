bd0d3794b6835feb19d2ada39ba5b262848bc79f
nicolesy <nicole@nicolesy.com>
2019-12-01 18:43:16 -0800

    Changed domain to 'login' (old name was 'user')

--- looking_glass/urls.py
+++ looking_glass/urls.py
@@ -7,5 +7,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('lg_app.urls')),
-    path('user', include('users.urls')),
+    path('login/', include('users.urls')), #old name was 'user'
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
