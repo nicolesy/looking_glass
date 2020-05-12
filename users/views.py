c39746e54fc875cdb76b1286fd85dcb7a812696b
Nicole Young <nicole@nicolesy.com>
2020-05-12 12:12:31 -0700

    Added "message" to def login_user else statement.

--- users/views.py
+++ users/views.py
@@ -13,27 +13,32 @@ def register_user(request):
     username = request.POST['username']
     email = request.POST['email']
     password = request.POST['password']
-    
+
     if User.objects.filter(username=username).exists():
         message = {
             "error": "Username and/or email are already taken.",
         }
         return render(request, 'users/login.html', message)
         # return HttpResponseRedirect(reverse('users:login_register'))
-    else:    
+    else:
         user = User.objects.create_user(username, email, password)
         login(request, user)
         return HttpResponseRedirect(reverse('lg_app:index'))
-    
+
+
 def login_user(request):
     username = request.POST['username']
     password = request.POST['password']
     user = authenticate(request, username=username, password=password)
-    if user is not None: # username and password matched a user
+    if user is not None:  # username and password matched a user
         login(request, user)
         return HttpResponseRedirect(reverse('lg_app:index'))
     else:
+        message = {
+            "error": "Incorrect password, or you are not yet registered.",
+        }
         return HttpResponseRedirect(reverse('users:login_register'))
+
 
 def logout_user(request):
     logout(request)
